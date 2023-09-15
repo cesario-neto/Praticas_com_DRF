2ac225fb36d8a6e1614e6b1896bc9ae4bda780af
Cesario_Neto <cesarioneto7@gmail.com>
2023-09-15 10:38:20 -0300

    Creating models and adding to admin

--- bookshelves/admin.py
+++ bookshelves/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import BookShelf, ImportBookShelf
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from utils.gender_imports import gender_import
 
-# Register your models here.
+
+@admin.register(BookShelf)
+class BookShelfAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    list_display_links = 'id', 'name',
+    search_fields = 'name',
+
+
+@admin.register(ImportBookShelf)
+class ImportBookShelfAdmin(admin.ModelAdmin):
+    list_display = 'id', 'file', 'finish',
+    list_display_links = 'id', 'file',
+
+
+@receiver(post_save, sender=ImportBookShelf)
+def import_gender(sender: BookShelf, **kwargs):
+    obj = sender.objects.filter(finish=False).first()
+    gender_import(obj, BookShelf)
--- bookshelves/models.py
+++ bookshelves/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BookShelf(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ImportBookShelf(models.Model):
+    file = models.FileField(upload_to='imports/bookshelf/%Y/%m')
+    finish = models.BooleanField(default=False)
